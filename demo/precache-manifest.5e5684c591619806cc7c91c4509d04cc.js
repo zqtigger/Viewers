e43be24b892a703760c45fd37e4f05ea18cf7ccc
npm gh-pages <gh-pages@localhost>
2019-06-28 14:29:17 +0000

    Autogenerated Message: [ci skip]

---
+++ demo/precache-manifest.5e5684c591619806cc7c91c4509d04cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d290bd300ee4d0e7407b4053264916",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "f8f6d38e44904bd552f3",
+    "url": "/demo/static/css/main.2642c8bf.chunk.css"
+  },
+  {
+    "revision": "4cd44e24a9f0f78f4c21",
+    "url": "/demo/static/js/2.9b4cdbbe.chunk.js"
+  },
+  {
+    "revision": "f8f6d38e44904bd552f3",
+    "url": "/demo/static/js/main.ab0cb0f5.chunk.js"
+  },
+  {
+    "revision": "2f23047b078009b94f5f",
+    "url": "/demo/static/js/runtime~main.96f7b30f.js"
+  }
+]);
--- demo/service-worker.js
+++ demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo/precache-manifest.59cb236433512ed11e8eada3fc7c0de1.js"
+  "/demo/precache-manifest.5e5684c591619806cc7c91c4509d04cc.js"
 );
 
 self.addEventListener('message', (event) => {
