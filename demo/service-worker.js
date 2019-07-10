60c9170d51c94db33e1abb8f84cac460c7ae92a3
npm gh-pages <gh-pages@localhost>
2019-07-10 07:41:54 +0000

    Autogenerated Message: [ci skip]

--- demo/service-worker.js
+++ demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/demo/precache-manifest.58bc1d1884a9ee3e52f41c1958433ad2.js"
+  "/demo/precache-manifest.350da47ce836854c73a736b06154afb6.js"
 );
 
 self.addEventListener('message', (event) => {
