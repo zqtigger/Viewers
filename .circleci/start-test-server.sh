33156d85e026a4647107b6562ed513c9501b208a
Erik Ziegler <erik.sweed@gmail.com>
2019-12-12 10:46:33 +0100

    Add CouchDB Initial setup

--- .circleci/start-test-server.sh
+++ .circleci/start-test-server.sh
@@ -9,6 +9,11 @@ git clone git://github.com/ohif/viewer-testdata ./test-server/
 
 cd test-server/dcm
 
+# Setup CouchDB
+curl -X PUT http://localhost:5984/_users
+curl -X PUT http://localhost:5984/_replicator
+curl -X PUT http://localhost:5984/_global_changes
+
 # Setup Python client
 apt-get -y install python2.7 python-pip
 pip install dicomweb-client
